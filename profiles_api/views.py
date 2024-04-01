123d404599c3de25f8e020407440bfa4ee4c26e3
Abdullah Kassas <a.kassas@gmail.com>
2024-04-01 00:13:56 -0400

    finished viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from profiles_api import serializers
 
 
@@ -47,3 +48,48 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'Delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+    
+    def list(self, request):
+        a_viewset = [
+            'a','v','vv',
+        ]
+
+        return Response({'message':'hahaha','a_viewset':a_viewset})
+
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"hello {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
+
+    def retrieve(self, request, pk=None):
+        """Handle getting by ID"""
+        return Response({'method':'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle Updating by ID"""
+        return Response({'method':'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating partially by ID"""
+        return Response({'method':'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Handle deleting by ID"""
+        return Response({'method':'DELETE'})
